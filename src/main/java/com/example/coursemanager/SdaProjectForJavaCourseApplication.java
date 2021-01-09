479a69caa62530117137fa3545347b28eb518014
wrb <wroblewski.lukasz91@gmail.com>
2021-01-09 10:27:42 +0100

    commit new project

---
+++ src/main/java/com/example/coursemanager/SdaProjectForJavaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaProjectForJavaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaProjectForJavaCourseApplication.class, args);
+	}
+
+}
