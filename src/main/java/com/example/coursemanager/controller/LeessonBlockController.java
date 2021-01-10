8d985a16c01950cc7c93501043e1cf7c77d9a1b6
wrb <wroblewski.lukasz91@gmail.com>
2021-01-10 09:55:27 +0100

    added lessonblockcontroller implementation

--- src/main/java/com/example/coursemanager/controller/LeessonBlockController.java
+++ src/main/java/com/example/coursemanager/controller/LeessonBlockController.java
@@ -1,10 +1,22 @@
 package com.example.coursemanager.controller;
 
+import com.example.coursemanager.model.LessonBlock;
+import com.example.coursemanager.repository.LessonBlockRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@AllArgsConstructor
 public class LeessonBlockController {
 
+    private final LessonBlockRepository lessonBlockRepository;
 
-
+    @GetMapping("/lessonblocks")
+    public List<LessonBlock> getLessonBlocks() {
+        return lessonBlockRepository.findAll().stream().collect(Collectors.toList());
+    }
 }
